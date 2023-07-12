ef6262b3049849c22978336e42f6df29a1d88c84
Lavesh-modi <laveshmodiag@gmail.com>
2023-07-12 18:52:50 +0530

    created basic navbar

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
+// function App() {
+//   return (
+//     <div classNameName="App">
+//       <header classNameName="App-header">
+//         <img src={logo} classNameName="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           classNameName="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Heading from "./Heading";
+// import List from "./List";
+
+// import Para from "./Para";
+// const Fav = "Lavesh";
+// const favorg = "React js";
+
+// function App() {
+//   return (
+//     <>
+//       <Heading />
+//       <Para />
+//       <List />
+//       {Fav}
+//     </>
+//   );
+// }
+
+// export default App;
+
+// export { Fav };
+// export {favorg};
+
+import "./App.css";
+import Navbar from "./component/Navbar";
+import Textform from "./component/Textform";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar tittle="My Pet" Aboutus="Aboutusss" />
+
+      <div className="container">
+        <Textform
+          heading="Enter the text to analyze "
+          inbox="Enter the value  "
+        />
+      </div>
+    </>
   );
 }
-
 export default App;
--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -107,31 +107,205 @@
 // );
 
 // mini project of date and wishes
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
 
-let currDate = new Date(); // new Date is not write in the new date is not defined
-currDate = currDate.getHours(2022, 7, 7, 17);
-let greeting = " "; // camelCase issue
-const cssStyle = {};
+// let currDate = new Date(); // new Date is not write in the new date is not defined
+// currDate = currDate.getHours(2022, 7, 7, 17);
+// let greeting = " "; // camelCase issue
+// const cssStyle = {};
+// let currTime = new Date().toLocaleTimeString();
 
-if (currDate >= 1 && currDate < 12) {
-  greeting = "Good Morning ";
-  cssStyle.color = "green";
-} else if (currDate >= 12 && currDate < 19) {
-  greeting = "Good Afternoon";
-  cssStyle.color = "orange";
-} // issue in the condition
-else greeting = "Good Night ";
-cssStyle.color = "blue"; 
+// if (currDate >= 1 && currDate < 12) {
+//   greeting = "Good Morning ";
+//   cssStyle.color = "green";
+// } else if (currDate >= 12 && currDate < 19) {
+//   greeting = "Good Afternoon";
+//   cssStyle.color = "orange";
+// } // issue in the condition
+// else greeting = "Good Night ";
+// cssStyle.color = "blue";
+// ReactDOM.render(
+//   <React.Fragment>
+//     <div className="flip">
+//       <h1>
+//         Hello sir , {currDate} {currTime};
+//         <span style={cssStyle}> {greeting} </span>
+//       </h1>
+//     </div>
+//   </React.Fragment>,
+//   document.getElementById("root")
+// );
+
+// now we are moving on the component
+
+// import React from "react";
+// import ReactDom from "react-dom";
+// import Heading from "./Heading";
+// import List from "./List";
+
+// import Para from "./Para";
+
+// ReactDom.render(
+//   <>
+//     <Heading />
+//     <Para />
+//     <List />
+//     <p>the top 5 netflix series</p>
+//     <ul>
+//       <li>Peaky Blender</li>
+//       <li>Ozark</li>
+//       <li>You</li>
+//       <li>Black Mirror</li>
+//       <li>The Last Kingdom</li>
+//     </ul>
+//   </>,
+//   document.getElementById("root")
+// );
+// import App, { favorg } from "./App";
+
+// ReactDom.render(
+//   <>
+//     <App />,{favorg}
+//   </>,
+//   document.getElementById("root")
+// );
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import {add, divi, mult, sub } from "./Calc";
+
+// ReactDOM.render(
+//   <>
+//     <ul>
+//       <li>sum of two no. is {add(40, 4)}</li>
+//       <li>sub of two no. is :{sub(30, 3)}</li>
+//       <li>multi of two no. is :{mult(10, 3)}</li>
+//       <li>Division of two no. is :{divi(30, 3)}</li>
+//     </ul>
+//   </>,
+//   document.getElementById("root")
+// );
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Card from "./Card";
+// import "./index.css";
+// import Sdata from "./Sdata";
+
+// we can write the data in the array format import the file in it
+// function Card(prop) {
+//   return (
+//     <>
+//       <div className="card1">
+//         <div className="card2">
+//           <img src={prop.imgsrc} alt="1899" className="img1" />
+//           <div className="card_info">
+//             <span className="card_platform">{prop.sname}</span>
+//             <h1 className="card_tittle">{prop.tittle}</h1>
+//             <a href={prop.link} alt="_blank  ">
+//               <button>Watch Now</button>
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// cghrfhn
+
+
+  /* {/* <Card
+      imgsrc="https://wallpapercave.com/wp/wp11766166.jpg"
+      tittle="1899"
+      sname="A Netflix Original Series"
+      link="https://www.netflix.com/in/title/80214497?source=35"
+    /> */
+
+/* <Card 
+      imgsrc={Sdata[0].imgsrc}
+      tittle={Sdata[0].tittle}
+      sname={Sdata[0].sname}
+      link={Sdata[0].link}
+    />
+    <Card
+      imgsrc="https://e0.pxfuel.com/wallpapers/457/603/desktop-wallpaper-season-4-of-ozark-soon-on-netflix.jpg"
+      tittle="Ozark"
+      sname="A Netflix Original Series"
+      link="https://www.netflix.com/in/title/80117552?source=35" */
+// </>  ///>
+/* <Card
+        imgsrc="https://wallpapers.com/images/featured/money-heist-segtwbhffwy01w82.webp"
+        tittle="Money Heist "
+        sname="A Netflix Original Series"
+        link="https://www.netflix.com/in/title/80192098?source=35"
+      {/* /> */
+// {/* <Card
+//   imgsrc="https://w0.peakpx.com/wallpaper/615/720/HD-wallpaper-thomas-shelby-blinders-netflix-peaky-peaky-blinders.jpg"
+//   tittle="Peaky Blinders"
+//   sname="A Netflix Original Series"
+//   link="https://www.netflix.com/in/title/80002479?source=35"
+// />
+
+// ReactDOM.render
+//   <>
+//     <h1 className="heading"> List of Top Five Netflix show in 2023</h1>
+//   </>,
+
+//   document.getElementById("root")
+// );
+
+// the map() method create new array with result of calling a function for every array element
+// array.map(function(currentvalue,index,arr)thisvalue)
+
+// const oldarr = ["Lavesh ","Kumar ","Modi"];
+// console.log(oldarr[0])
+// console.log(oldarr[1])
+
+// console.log(oldarr[2])
+
+// const newarr = oldarr.map(function (cvalue)
+// {
+//     return cvalue + "";
+// }
+// );
+// console.log(newarr);
+
+// /
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Pratice from "./pratice";
+
+// ReactDOM.render(
+//   <>
+//     <Pratice name="Yashika "></Pratice>
+//   </>,
+//   document.getElementById("root")
+// );
+
+
+// import React from "react";
+// import  ReactDOM  from "react-dom";
+//  import Comment from "./comment";
+//  import User from "./User";
+//  <Comment >,
+//  document.getElementById("root") 
+// import React,{useState} from "react"
+// import  ReactDOM  from "react-dom"
+
+// fuinction click(){
+
+
+ import React from "react";
+import  ReactDOM  from "react-dom";
+import App from "./App";
+// import "./App";
+
 ReactDOM.render(
-  <React.Fragment>
-    <div className="flip">
-      <h1>
-        Hello sir , {currDate} <span style={cssStyle}> {greeting} </span>
-      </h1>
-    </div>
-  </React.Fragment>,
+  <>
+    <App/>
+  </>,
   document.getElementById("root")
-);
+)
+  
